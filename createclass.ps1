6d335cbe5334bb2a554c14ed0c1e2b98496014e6
Chaoschaps <74681462+Chaoschaps@users.noreply.github.com>
2021-03-31 07:31:47 +0200

    Add files via upload

--- createclass.ps1
+++ createclass.ps1
@@ -1,31 +1,4 @@
-# Function createclassc {
-#     [CmdletBinding()]
-#     param (
-#         [Parameter(Mandatory = $true)][string]$name,
-#         [switch]$nodir
-#     )
-    
-#     if ($nodir) {
-#         New-Item -Path . -Name "$name.h" -ItemType "file" -Value "#ifndef _$name`_h_ `n#define _$name`_h_ `nstruct $name{`n`n};`n#endif"
-#         New-Item -Path . -Name "$name.c" -ItemType "file" -Value "#include `"$name.h`""
-#     }
-#     else {
-#         if (Test-Path -Path .\include) {
-#             New-Item -Path .\include -Name "$name.h" -ItemType "file" -Value "#ifndef _$name`_h_ `n#define _$name`_h_ `nstruct $name{`n`n};`n#endif"
-#         }
-#         if (Test-Path -Path .\src) {
-#             New-Item -Path .\src -Name "$name.c" -ItemType "file" -Value "#include `"$name.h`"" 
-#         }
-#         else {
-#             New-Item -Path . -Name "src" -ItemType "directory"
-#             New-Item -Path . -Name "include" -ItemType "directory"
-#             New-Item -Path .\include -Name "$name.h" -ItemType "file" -Value "#ifndef _$name`_h_ `n#define _$name`_h_ `nstruct $name{`n`n};`n#endif"
-#             New-Item -Path .\src -Name "$name.c" -ItemType "file" -Value "#include `"$name.h`""
-#         }
-#     }
-# }
-
-Function createclasscpp {
+Function createclass {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)][string]$name,
